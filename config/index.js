4545f1f7db9bd7e3454ee16c45cdf2a85debe514
J-Z0807 <s17113209@stu.edu.tw>
2020-11-19 14:32:33 +0800

    路徑設定

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/CLI-todolist/',
+    assetsPublicPath: '/CLI-todolist/dist',
     proxyTable: {},
 
     // Various Dev Server settings
