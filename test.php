0d859c78246487b9417e592ba0be577064ce7f29
J-Z0807 <s17113209@stu.edu.tw>
2020-11-21 17:28:15 +0800

    更改

--- test.php
+++ test.php
@@ -1,2 +1,26 @@
 <?php
-echo "sad";
+      //連線變數設定
+      $DBHOST = "localhost";
+      $DBUSER = "root";
+      $DBPASSWD = "";
+      $DBNAME = "todolist";
+
+      $conn = mysqli_connect( $DBHOST, $DBUSER, $DBPASSWD, $DBNAME); //連線語法
+      if (empty($conn)){
+        print mysqli_error($conn);
+        die ("新增失敗");
+        exit;
+      }
+      else {
+        echo "已新增";
+      }
+
+      //將字符集設成 'utf8'
+      mysqli_set_charset($conn,'utf8');
+
+    //   $sql = "insert into student values ('$studentID','$NAME');";
+
+    //   mysqli_query($conn, $sql);
+
+      mysqli_close($conn);
+    ?>
